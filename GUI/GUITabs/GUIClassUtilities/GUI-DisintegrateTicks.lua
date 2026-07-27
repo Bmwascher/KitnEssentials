48ed7b07764750e184dbcf10f19ce3a8bd943b05
Brandon Wascher <bmwascher97@gmail.com>
2026-07-27 09:35:20 -0500

    convert evoker module enables to card header toggles

--- GUI/GUITabs/GUIClassUtilities/GUI-DisintegrateTicks.lua
+++ GUI/GUITabs/GUIClassUtilities/GUI-DisintegrateTicks.lua
@@ -59,22 +59,11 @@ GUIFrame:RegisterContent("DisintegrateTicks", function(scrollChild, yOffset)
     -- Card 1: Enable
     ----------------------------------------------------------------
     local card1 = GUIFrame:CreateCard(scrollChild, "Disintegrate Ticks", yOffset)
-
-    local row1 = GUIFrame:CreateRow(card1.content, Theme.rowHeight)
-    local enableCheck = GUIFrame:CreateCheckbox(row1, "Enable Disintegrate Ticks", {
-        value = db.Enabled ~= false,
-        callback = function(checked)
-            db.Enabled = checked
-            ApplyModuleState(checked)
-            RefreshStates()
-        end,
-        msgPopup = true,
-        msgText = "Disintegrate Ticks",
-        msgOn = "On",
-        msgOff = "Off",
-    })
-    row1:AddWidget(enableCheck, 0.5)
-    card1:AddRow(row1, Theme.rowHeight)
+    card1:AddHeaderToggle(db.Enabled ~= false, function(checked)
+        db.Enabled = checked
+        ApplyModuleState(checked)
+        KE:Print("Disintegrate Ticks: " .. (checked and "|cff4DCC66On|r" or "|cffE64D4DOff|r"))
+    end)
 
     local noteRow = GUIFrame:CreateRow(card1.content, 50)
     local noteText = GUIFrame:CreateText(noteRow,
@@ -86,6 +75,9 @@ GUIFrame:RegisterContent("DisintegrateTicks", function(scrollChild, yOffset)
     card1:AddRow(noteRow, 50, 0)
 
     yOffset = card1:GetNextOffset()
+
+    -- Lone header bar: a disabled module shows its switch and nothing else.
+    if db.Enabled == false then return yOffset end
 
     ----------------------------------------------------------------
     -- Card 2: Tick Settings
--- GUI/GUITabs/GUIClassUtilities/GUI-EbonMightHelper.lua
+++ GUI/GUITabs/GUIClassUtilities/GUI-EbonMightHelper.lua
@@ -54,22 +54,11 @@ GUIFrame:RegisterContent("EbonMightHelper", function(scrollChild, yOffset)
     -- Card 1: Enable
     ----------------------------------------------------------------
     local card1 = GUIFrame:CreateCard(scrollChild, "Ebon Might Extension Helper", yOffset)
-
-    local row1 = GUIFrame:CreateRow(card1.content, Theme.rowHeight)
-    local enableCheck = GUIFrame:CreateCheckbox(row1, "Enable Ebon Might Helper", {
-        value = db.Enabled ~= false,
-        callback = function(checked)
-            db.Enabled = checked
-            ApplyModuleState(checked)
-            RefreshStates()
-        end,
-        msgPopup = true,
-        msgText = "Ebon Might Helper",
-        msgOn = "On",
-        msgOff = "Off",
-    })
-    row1:AddWidget(enableCheck, 0.5)
-    card1:AddRow(row1, Theme.rowHeight)
+    card1:AddHeaderToggle(db.Enabled ~= false, function(checked)
+        db.Enabled = checked
+        ApplyModuleState(checked)
+        KE:Print("Ebon Might Helper: " .. (checked and "|cff4DCC66On|r" or "|cffE64D4DOff|r"))
+    end)
 
     local noteRow = GUIFrame:CreateRow(card1.content, 65)
     local noteText = GUIFrame:CreateText(noteRow,
@@ -82,6 +71,9 @@ GUIFrame:RegisterContent("EbonMightHelper", function(scrollChild, yOffset)
     card1:AddRow(noteRow, 65, 0)
 
     yOffset = card1:GetNextOffset()
+
+    -- Lone header bar: a disabled module shows its switch and nothing else.
+    if db.Enabled == false then return yOffset end
 
     ----------------------------------------------------------------
     -- Card 2: Sound Settings
