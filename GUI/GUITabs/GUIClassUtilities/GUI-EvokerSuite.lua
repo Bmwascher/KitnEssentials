afa0ea9ccbc1cfa858d6d5e375d84fe0e35a0676
Brandon Wascher <bmwascher97@gmail.com>
2026-07-27 00:24:09 -0500

    convert evoker suite to the shared tabbed content mechanism

--- GUI/GUITabs/GUIClassUtilities/GUI-EvokerSuite.lua
+++ GUI/GUITabs/GUIClassUtilities/GUI-EvokerSuite.lua
@@ -8,25 +8,9 @@
 local KE = select(2, ...)
 local GUIFrame = KE.GUIFrame
 
-local activeTab = "DisintegrateTicks"
-
-GUIFrame:RegisterContent("EvokerSuite", function(scrollChild, yOffset)
-    local _, newOffset = GUIFrame:CreateSubTabs(scrollChild, yOffset, {
-        tabs = {
-            { id = "DisintegrateTicks", label = "Disintegrate" },
-            { id = "StasisTracker",     label = "Stasis" },
-            { id = "EbonMightHelper",   label = "Ebon Might" },
-            { id = "PrescienceTracker", label = "Prescience" },
-        },
-        activeId = activeTab,
-        onSwitch = function(newId) activeTab = newId end,
-        fill = true,
-    })
-    yOffset = newOffset
-
-    local builder = GUIFrame.registeredContent[activeTab]
-    if builder then
-        return builder(scrollChild, yOffset)
-    end
-    return yOffset
-end)
+GUIFrame:RegisterTabbedContent("EvokerSuite", {
+    { id = "DisintegrateTicks", label = "Disintegrate" },
+    { id = "StasisTracker",     label = "Stasis" },
+    { id = "EbonMightHelper",   label = "Ebon Might" },
+    { id = "PrescienceTracker", label = "Prescience" },
+})
