a027c7b7fc6961e3f70c6ff89fabc1a49603c621
Brandon Wascher <bmwascher97@gmail.com>
2026-09-03 23:12:10 -0500

    put the secondary stats cards and sidebar entry in house order

--- GUI/GUITabs/GUIQoL/GUI-SecondaryStats.lua
+++ GUI/GUITabs/GUIQoL/GUI-SecondaryStats.lua
@@ -68,10 +68,52 @@ GUIFrame:RegisterContent("SecondaryStats", function(scrollChild, yOffset)
     if db.Enabled == false then return yOffset end
 
     ----------------------------------------------------------------
-    -- Card 2: Stats to show
+    -- Card 2: Position Settings
     ----------------------------------------------------------------
-    local card2 = GUIFrame:CreateCard(scrollChild, "Stats to Show", yOffset)
-    manager:Register(card2, "all")
+    local posCard, posOffset = GUIFrame:CreatePositionCard(scrollChild, yOffset, {
+        title = "Position Settings",
+        db = db,
+        dbKeys = {
+            selfPoint = "AnchorFrom",
+            anchorPoint = "AnchorTo",
+            xOffset = "XOffset",
+            yOffset = "YOffset",
+        },
+        showAnchorFrameType = true,
+        showStrata = true,
+        onChangeCallback = ApplySettings,
+    })
+
+    if posCard.positionWidgets then
+        manager:RegisterGroup(posCard.positionWidgets, "all")
+    end
+    manager:Register(posCard, "all")
+    yOffset = posOffset
+
+    ----------------------------------------------------------------
+    -- Card 3: Font Settings
+    ----------------------------------------------------------------
+    local fontCard, fontOffset, fontWidgets = GUIFrame:CreateFontSettingsCard(scrollChild, yOffset, {
+        db = db,
+        dbKeys = {
+            fontFace = "FontFace",
+            fontSize = "FontSize",
+            fontOutline = "FontOutline",
+        },
+        fontSizeRange = { 8, 32 },
+        onChangeCallback = ApplySettings,
+    })
+    manager:Register(fontCard, "all")
+    if fontWidgets then
+        manager:RegisterGroup(fontWidgets, "all")
+    end
+    yOffset = fontOffset
+
+    ----------------------------------------------------------------
+    -- Card 4: Stats to Show
+    ----------------------------------------------------------------
+    local card4 = GUIFrame:CreateCard(scrollChild, "Stats to Show", yOffset)
+    manager:Register(card4, "all")
 
     -- Ordered arrays, not maps: the dropdown sorts a map by key, which would
     -- list Both before Percent and put None first in the separator list.
@@ -110,7 +152,7 @@ GUIFrame:RegisterContent("SecondaryStats", function(scrollChild, yOffset)
         local key = STAT_KEYS[index]
         local isLast = index == #STAT_KEYS
         local rowHeight = isLast and Theme.rowHeightLast or Theme.rowHeight
-        local row = GUIFrame:CreateRow(card2.content, rowHeight)
+        local row = GUIFrame:CreateRow(card4.content, rowHeight)
 
         local shownCheck = GUIFrame:CreateCheckbox(row, STAT_TEXT[key], {
             value = db.Stats[key].Shown == true,
@@ -133,19 +175,19 @@ GUIFrame:RegisterContent("SecondaryStats", function(scrollChild, yOffset)
         row:AddWidget(modeDropdown, 0.5)
         manager:Register(modeDropdown, "all")
 
-        card2:AddRow(row, rowHeight, isLast and 0 or nil)
+        card4:AddRow(row, rowHeight, isLast and 0 or nil)
     end
 
-    yOffset = card2:GetNextOffset()
+    yOffset = card4:GetNextOffset()
 
     ----------------------------------------------------------------
-    -- Card 3: Text
+    -- Card 5: Text
     ----------------------------------------------------------------
-    local card3 = GUIFrame:CreateCard(scrollChild, "Text", yOffset)
-    manager:Register(card3, "all")
+    local card5 = GUIFrame:CreateCard(scrollChild, "Text", yOffset)
+    manager:Register(card5, "all")
 
-    local row3a = GUIFrame:CreateRow(card3.content, Theme.rowHeight)
-    local labelDropdown = GUIFrame:CreateDropdown(row3a, "Label Style", {
+    local row5a = GUIFrame:CreateRow(card5.content, Theme.rowHeight)
+    local labelDropdown = GUIFrame:CreateDropdown(row5a, "Label Style", {
         options = labelStyleList,
         value = db.LabelStyle or "full",
         callback = function(selected)
@@ -153,10 +195,10 @@ GUIFrame:RegisterContent("SecondaryStats", function(scrollChild, yOffset)
             ApplySettings()
         end,
     })
-    row3a:AddWidget(labelDropdown, 0.5)
+    row5a:AddWidget(labelDropdown, 0.5)
     manager:Register(labelDropdown, "all")
 
-    local sepDropdown = GUIFrame:CreateDropdown(row3a, "Separator", {
+    local sepDropdown = GUIFrame:CreateDropdown(row5a, "Separator", {
         options = separatorList,
         value = db.Separator or ":",
         callback = function(selected)
@@ -164,12 +206,12 @@ GUIFrame:RegisterContent("SecondaryStats", function(scrollChild, yOffset)
             ApplySettings()
         end,
     })
-    row3a:AddWidget(sepDropdown, 0.5)
+    row5a:AddWidget(sepDropdown, 0.5)
     manager:Register(sepDropdown, "all")
-    card3:AddRow(row3a, Theme.rowHeight)
+    card5:AddRow(row5a, Theme.rowHeight)
 
-    local row3b = GUIFrame:CreateRow(card3.content, Theme.rowHeight)
-    local decimalSlider = GUIFrame:CreateSlider(row3b, "Decimal Places", {
+    local row5b = GUIFrame:CreateRow(card5.content, Theme.rowHeight)
+    local decimalSlider = GUIFrame:CreateSlider(row5b, "Decimal Places", {
         min = 0, max = 3, step = 1,
         value = db.Decimals or 2,
         callback = function(val)
@@ -177,10 +219,10 @@ GUIFrame:RegisterContent("SecondaryStats", function(scrollChild, yOffset)
             ApplySettings()
         end,
     })
-    row3b:AddWidget(decimalSlider, 0.5)
+    row5b:AddWidget(decimalSlider, 0.5)
     manager:Register(decimalSlider, "all")
 
-    local scaleSlider = GUIFrame:CreateSlider(row3b, "Scale", {
+    local scaleSlider = GUIFrame:CreateSlider(row5b, "Scale", {
         min = 0.5, max = 2.0, step = 0.05,
         value = db.Scale or 1.0,
         callback = function(val)
@@ -188,12 +230,12 @@ GUIFrame:RegisterContent("SecondaryStats", function(scrollChild, yOffset)
             ApplySettings()
         end,
     })
-    row3b:AddWidget(scaleSlider, 0.5)
+    row5b:AddWidget(scaleSlider, 0.5)
     manager:Register(scaleSlider, "all")
-    card3:AddRow(row3b, Theme.rowHeight)
+    card5:AddRow(row5b, Theme.rowHeight)
 
-    local row3c = GUIFrame:CreateRow(card3.content, Theme.rowHeightLast)
-    local gapSlider = GUIFrame:CreateSlider(row3c, "Row Spacing", {
+    local row5c = GUIFrame:CreateRow(card5.content, Theme.rowHeightLast)
+    local gapSlider = GUIFrame:CreateSlider(row5c, "Row Spacing", {
         min = 0, max = 12, step = 1,
         value = db.RowGap or 3,
         callback = function(val)
@@ -201,20 +243,20 @@ GUIFrame:RegisterContent("SecondaryStats", function(scrollChild, yOffset)
             ApplySettings()
         end,
     })
-    row3c:AddWidget(gapSlider, 1)
+    row5c:AddWidget(gapSlider, 1)
     manager:Register(gapSlider, "all")
-    card3:AddRow(row3c, Theme.rowHeightLast, 0)
+    card5:AddRow(row5c, Theme.rowHeightLast, 0)
 
-    yOffset = card3:GetNextOffset()
+    yOffset = card5:GetNextOffset()
 
     ----------------------------------------------------------------
-    -- Card 4: Colors
+    -- Card 6: Colors
     ----------------------------------------------------------------
-    local card4 = GUIFrame:CreateCard(scrollChild, "Colors", yOffset)
-    manager:Register(card4, "all")
+    local card6 = GUIFrame:CreateCard(scrollChild, "Colors", yOffset)
+    manager:Register(card6, "all")
 
-    local row4a = GUIFrame:CreateRow(card4.content, Theme.rowHeight)
-    local colorDropdown = GUIFrame:CreateDropdown(row4a, "Label Color", {
+    local row6a = GUIFrame:CreateRow(card6.content, Theme.rowHeight)
+    local colorDropdown = GUIFrame:CreateDropdown(row6a, "Label Color", {
         options = colorModeList,
         value = db.ColorMode or "palette",
         callback = function(selected)
@@ -222,22 +264,22 @@ GUIFrame:RegisterContent("SecondaryStats", function(scrollChild, yOffset)
             ApplySettings()
         end,
     })
-    row4a:AddWidget(colorDropdown, 0.5)
+    row6a:AddWidget(colorDropdown, 0.5)
     manager:Register(colorDropdown, "all")
 
-    local customPicker = GUIFrame:CreateColorPicker(row4a, "Custom Label Color", {
+    local customPicker = GUIFrame:CreateColorPicker(row6a, "Custom Label Color", {
         color = db.CustomColor,
         callback = function(r, g, b)
             db.CustomColor = { r, g, b }
             ApplySettings()
         end,
     })
-    row4a:AddWidget(customPicker, 0.5)
+    row6a:AddWidget(customPicker, 0.5)
     manager:Register(customPicker, "all")
-    card4:AddRow(row4a, Theme.rowHeight)
+    card6:AddRow(row6a, Theme.rowHeight)
 
-    local row4b = GUIFrame:CreateRow(card4.content, Theme.rowHeight)
-    local tertDropdown = GUIFrame:CreateDropdown(row4b, "Leech / Avoidance / Speed Color", {
+    local row6b = GUIFrame:CreateRow(card6.content, Theme.rowHeight)
+    local tertDropdown = GUIFrame:CreateDropdown(row6b, "Leech / Avoidance / Speed Color", {
         options = tertiaryModeList,
         value = db.TertiaryColorMode or "class",
         callback = function(selected)
@@ -245,75 +287,33 @@ GUIFrame:RegisterContent("SecondaryStats", function(scrollChild, yOffset)
             ApplySettings()
         end,
     })
-    row4b:AddWidget(tertDropdown, 0.5)
+    row6b:AddWidget(tertDropdown, 0.5)
     manager:Register(tertDropdown, "all")
 
-    local tertPicker = GUIFrame:CreateColorPicker(row4b, "Custom Tertiary Color", {
+    local tertPicker = GUIFrame:CreateColorPicker(row6b, "Custom Tertiary Color", {
         color = db.TertiaryColor,
         callback = function(r, g, b)
             db.TertiaryColor = { r, g, b }
             ApplySettings()
         end,
     })
-    row4b:AddWidget(tertPicker, 0.5)
+    row6b:AddWidget(tertPicker, 0.5)
     manager:Register(tertPicker, "all")
-    card4:AddRow(row4b, Theme.rowHeight)
+    card6:AddRow(row6b, Theme.rowHeight)
 
-    local row4c = GUIFrame:CreateRow(card4.content, Theme.rowHeightLast)
-    local coloredCheck = GUIFrame:CreateCheckbox(row4c, "Color the Numbers Too", {
+    local row6c = GUIFrame:CreateRow(card6.content, Theme.rowHeightLast)
+    local coloredCheck = GUIFrame:CreateCheckbox(row6c, "Color the Numbers Too", {
         value = db.ColoredValues == true,
         callback = function(checked)
             db.ColoredValues = checked
             ApplySettings()
         end,
     })
-    row4c:AddWidget(coloredCheck, 1)
+    row6c:AddWidget(coloredCheck, 1)
     manager:Register(coloredCheck, "all")
-    card4:AddRow(row4c, Theme.rowHeightLast, 0)
+    card6:AddRow(row6c, Theme.rowHeightLast, 0)
 
-    yOffset = card4:GetNextOffset()
-
-    ----------------------------------------------------------------
-    -- Card 5: Position Settings
-    ----------------------------------------------------------------
-    local posCard, posOffset = GUIFrame:CreatePositionCard(scrollChild, yOffset, {
-        title = "Position Settings",
-        db = db,
-        dbKeys = {
-            selfPoint = "AnchorFrom",
-            anchorPoint = "AnchorTo",
-            xOffset = "XOffset",
-            yOffset = "YOffset",
-        },
-        showAnchorFrameType = true,
-        showStrata = true,
-        onChangeCallback = ApplySettings,
-    })
-
-    if posCard.positionWidgets then
-        manager:RegisterGroup(posCard.positionWidgets, "all")
-    end
-    manager:Register(posCard, "all")
-    yOffset = posOffset
-
-    ----------------------------------------------------------------
-    -- Card 6: Font Settings
-    ----------------------------------------------------------------
-    local fontCard, fontOffset, fontWidgets = GUIFrame:CreateFontSettingsCard(scrollChild, yOffset, {
-        db = db,
-        dbKeys = {
-            fontFace = "FontFace",
-            fontSize = "FontSize",
-            fontOutline = "FontOutline",
-        },
-        fontSizeRange = { 8, 32 },
-        onChangeCallback = ApplySettings,
-    })
-    manager:Register(fontCard, "all")
-    if fontWidgets then
-        manager:RegisterGroup(fontWidgets, "all")
-    end
-    yOffset = fontOffset
+    yOffset = card6:GetNextOffset()
 
     RefreshStates()
     return yOffset
